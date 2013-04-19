45d5373549a85bf4831d2d5805a3e96024d4bfcb
John Engelhart <johne@syntellia.com>
2013-04-19 14:34:28 -0700

    Change `main.cpp` in the `samples` directory so that it automagically discovers the FULL path to `FleksySDKResources`.

--- samples/FleksySample1/src/main.cpp
+++ samples/FleksySample1/src/main.cpp
@@ -1,9 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/param.h>
+#include <string.h>
+#include <errno.h>
+#include <libgen.h>
+#include <unistd.h>
 
-#import "FleksyTester.h"
+#include "FleksyTester.h"
 
 int main(int argc, const char * argv[]) {
-  
-  FleksyTester::run("FleksySDKResources/encrypted/en-US/");
-  
+  char resolved_path[PATH_MAX];
+  if(realpath(argv[0], resolved_path) == 0) { fprintf(stderr, "Unable to resolve path to executable, realpath() failed: %s\n", strerror(errno)); exit(1); }
+
+  char FleksySDKResourcesPath[PATH_MAX];
+  snprintf(FleksySDKResourcesPath, sizeof(FleksySDKResourcesPath), "%s/FleksySDKResources/encrypted/en-US/", dirname(resolved_path));
+
+  FleksyTester::run(FleksySDKResourcesPath);
+
   return 0;
 }
