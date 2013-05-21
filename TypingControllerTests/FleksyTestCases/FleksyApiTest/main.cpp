79723714bba76bfe06e8cc9513ba6dee891cb391
John Engelhart <johne@syntellia.com>
2013-05-20 18:20:37 -0700

    Get `TypingControllerTests` working.
    NOTE: This is dependent on syntellia/FleksySDK-source@62791b383bd133c8286a2280e4bfa21b9c68f660!
    
    This commit does not include a new `FleksySDK-compiled` built from `FleksySDK-source` that incorporates those changes!

--- TypingControllerTests/FleksyTestCases/FleksyApiTest/main.cpp
+++ TypingControllerTests/FleksyTestCases/FleksyApiTest/main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include "FLTypingControllerTester.h"
 
-
 using namespace std;
 
 FLTypingControllerTester tester;
 
-int main(int argc, const char * argv[]){
-    tester.setup();
+int main(int argc, const char * argv[]) {
+  int returnResult = 1;
+
   try {
+    tester.setup();
     tester.runTests(false);
+    returnResult = 0;
   } catch (std::runtime_error er) {
     printf("Error while running tests: %s\n", er.what());
   } catch (std::exception ex) {
     printf("Exception while running tests: %s\n", ex.what());
-  }
-  catch(...){
-    printf("I'm here ERROR!");
-
+  } catch(...){
+    printf("I'm here ERROR!\n");
   }
   
-    return 0;
+  return returnResult;
 }
