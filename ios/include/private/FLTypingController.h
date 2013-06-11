388847d9354ffed62e5476eb681523547cd2afe8
Vadim Maximov <vadim@syntellia.com>
2013-06-11 12:35:06 -0700

    -

--- ios/include/private/FLTypingController.h
+++ ios/include/private/FLTypingController.h
@@ -42,12 +42,14 @@ public:
   void setActiveKeyboard(FLKeyboardID id, bool buttonPress);
   FLKeyboardID getActiveKeyboardID();
   void setCorrectionMode(FLCorrectionMode mode);
+  FLCorrectionMode getCorrectionMode();
   void cursorSelectionChanged(int selectionStart, int selectionEnd);
   void startTypingSession(bool platformMovesCursor = false);
   void endTypingSession();
   void postLoadSetup();
   void setPlatformSuggestions(FLString prossedRequestID, FLString processedResults);
   void setCapitalizationMode(FLCapitalizationMode mode);
+  FLCapitalizationMode getCapitalizationMode();
   void setVoiceFeedback(bool isOn);
   
   void setMaxNumberOfSuggestions(int numOfSuggestions); //Private API uses this
@@ -88,7 +90,7 @@ private:
   bool isShiftPressed;
   bool simulateSystemCursorUpdates; //true = simulation of updates, false - updates come from the system
   bool isUserSelectedTB; //indicates if user selectedTB by moving the cursor
-  bool autoCorrect;
+  FLCorrectionMode correctionMode;
   
   //TODO:rename this 
   bool isFirstInputAfterParsing;
@@ -150,7 +152,6 @@ private:
   void onBeginBatchEdit();
   void onEndBatchEdit();
   void batchEditCheck(int newSelStart, int newSelEnd);
-  void setAutoCorrect(bool isOn);
   
   //User cursor operations
   void moveCursorToPosition(int position);
