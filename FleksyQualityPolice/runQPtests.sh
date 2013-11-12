e2416a52935ba92ffae6eedf89bfbf872d6395b3
Vadim Maximov <vadim@syntellia.com>
2013-11-11 17:53:36 -0800

    returns all keys in keyboard file

---
+++ FleksyQualityPolice/runQPtests.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+if [[ "$#" != "1" ]]; then
+echo "Usage: $0 PATH_TO_ASSETS"
+echo ""
+exit 1
+fi
+
+
+for dir in $1/* ; do
+if [[ -d "$dir" && ! -L "$dir" ]]; then
+
+#echo "Directory: $dir"
+  FILES=$(find $dir -type f)
+
+  for file in $FILES;
+    do
+      if [ -f $file ];
+      then
+          SUBDIR="$(basename $dir)"
+          echo  $SUBDIR
+          java -Xmx2048M -jar FleksyTester.jar -ip -e -a s10 n20 locale:$SUBDIR
+      fi
+    done
+fi
+done
