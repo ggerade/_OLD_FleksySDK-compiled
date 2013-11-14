fc16bb4d08515e733c3a3a157ce86248d3f86f04
John Engelhart <johne@syntellia.com>
2013-11-14 12:54:32 -0800

    Take a big dump on Grey and blow his script away so we can run multiple quality police at once.

---
+++ FleksyQualityPolice/quality.mk
@@ -0,0 +1,17 @@
+FQP_ARGS ?= -ip -e -a -q s10 n20 w200
+
+ASSETS_LANGUAGES = bg-BG cs-CZ da-DK de-DE el-GR en-US es-ES et-EE fi-FI fr-FR hr-HR hu-HU it-IT lt-LT ms-MY nb-NO nl-NL pl-PL pt-BR pt-PT ro-RO ru-RU sk-SK sv-SE uk-UA
+# tr-TR
+
+.PHONY: all $(ASSETS_LANGUAGES)
+
+all: $(ASSETS_LANGUAGES)
+
+$(ASSETS_LANGUAGES): FleksyTester.jar
+	@/usr/bin/perl -C -e '$$FQP=`java -Xmx2048M -jar FleksyTester.jar $(FQP_ARGS) -loc:$@ 2>&1`; print("vvvvv $@ vvvvv\n" . $$FQP . "^^^^^ $@ ^^^^^^\n");'
+
+JAVA_SRCS = src/engine/Converter.java src/engine/Definer.java src/engine/FleksyEngine.java src/engine/NoiseMaker.java src/engine/Reader.java src/engine/TestEngine.java src/utils/Client.java src/utils/DataManager.java src/utils/Debugger.java src/utils/FleksyInterface.java src/utils/Key.java src/utils/Log.java src/utils/Point.java src/utils/Touch.java src/utils/Word.java src/views/FleksTest.java
+
+FleksyTester.jar: $(JAVA_SRCS) Scripts/buildFleksyTester.sh
+	@echo "Building FleksyTester..."
+	@Scripts/buildFleksyTester.sh
--- FleksyQualityPolice/runQPtests.sh
+++ FleksyQualityPolice/runQPtests.sh
@@ -1,33 +1,12 @@
 #!/bin/sh
 
-
-if [[ $# -lt 1 ]]; then
-echo "Usage: $0 PATH_TO_ASSETS"
-exit 1
+if [[ $# -gt 1 ]]; then
+    export FQP_ARGS="-ip -e -a -q $@"
+else
+    export FQP_ARGS="-ip -e -a -q s10 n20"
 fi
 
-for dir in $1/* ; do
-if [[ -d "$dir" && ! -L "$dir" ]]; then
+make -j`sysctl -n hw.ncpu` -e -f quality.mk all
 
-  LANG_CODE="$(basename $dir)"
-
-  FILES=$(find $dir -type f)
-
-  for file in $FILES;
-    do
-      if [ -f $file ];
-      then
-        FILE="$(basename $file)" 
-          #echo "LangCode: $LANG_CODE, File: $FILE"
-          if [[ $# -gt 1 ]];
-          then
-java -Xmx2048M -jar FleksyTester.jar -ip -e -a -q -fol:"$@" -loc:$LANG_CODE -fil:$FILE
-          else
-java -Xmx2048M -jar FleksyTester.jar -ip -e -a -q s10 n20 -fol:Assets -loc:$LANG_CODE -fil:$FILE
-          fi
-      fi
-    done
- fi
- done
-
+echo ""
 echo "Ding! Finished running all tests!"
