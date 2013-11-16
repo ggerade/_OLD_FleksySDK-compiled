608ebf84dd758548283fd0281bd33c124d8a17c4
greyski <grey@syntellia.com>
2013-11-15 17:22:49 -0800

    QP v8.7 add to dictionary print in debug

--- FleksyQualityPolice/src/engine/Reader.java
+++ FleksyQualityPolice/src/engine/Reader.java
@@ -26,8 +26,8 @@ public class Reader {
 			Log.e(TAG, "Error Reading File: " + e.toString());
 		}
 		String output = fileText.toString();
-    output = output.replaceAll("[\u00ab\u00bb\u201c\u201d\u201e\u201f\u300e\u300f\u301d\u301e\u301f\ufe43\ufe44\uff02]", "\"");
-    output = output.replaceAll("[\u0027\u2018\u2019\u201a\u201b\u2039\u203a\u300c\u300d\ufe41\ufe42\uff07\uff62\uff63]", "'");
+	    output = output.replaceAll("[\u00ab\u00bb\u201c\u201d\u201e\u201f\u300e\u300f\u301d\u301e\u301f\ufe43\ufe44\uff02]", "\"");
+	    output = output.replaceAll("[\u0027\u2018\u2019\u201a\u201b\u2039\u203a\u300c\u300d\ufe41\ufe42\uff07\uff62\uff63]", "'");
 		output = output.replaceAll("\u00AD",""); // Strip invisible soft-hyphens.
 		output = output.replaceAll("[\u2010\u2011\u2012\u2013\u2014\u2015\u2E3A\u2E3B\uFE31\uFE32\uFE58\uFE63\uFF0D]","\u002D"); // Turn crazy hyphens in to a regular - hyphen.
 		output = output.replaceAll("[\u00A0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000]","\u0020"); // Turn crazy spaces in to regular space.
