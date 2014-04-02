761f4ada9a913464128feed40bfff0e21cfd3328
Mike Loh <mike@syntellia.com>
2014-04-02 15:41:18 -0700

    adding quality analysis script and config
    modify sampleSentences to output and accept seed.

---
+++ Quality/runQualityAnalysis.sh
@@ -0,0 +1,67 @@
+function baseDir() 
+{
+    echo $(dirname $(cd $(dirname "$0"); pwd -P)/$(basename "$0"))
+}
+
+source $(baseDir)/../Scripts/common.sh
+source $(baseDir)/qualityAnalysis.config
+
+PYTHON3=/Library/Frameworks/Python.framework/Versions/3.3/bin/python3
+
+if [[ -z `type -t ${PYTHON3}` ]]; then
+    echo "/Library/Frameworks/Python.framework/Versions/3.3/bin/python3 not found"
+    exit
+fi
+
+FL_COMPILED_SCRIPT_PATH=$(canonPath "$0")
+FL_COMPILED_ROOT_DIR=`rootDir Quality`
+FL_COMPILED_SCRIPTS_DIR="${FL_COMPILED_ROOT_DIR}/Scripts"
+FL_COMPILED_QUALITY_DIR="${FL_COMPILED_ROOT_DIR}/Quality"
+
+FL_ASSETS_ROOT=${FL_COMPILED_ROOT_DIR}/FleksyQualityPolice
+FL_PARLIMENT_ASSET_DIR=${FL_ASSETS_ROOT}/AssetsParliament/${LANG}-${COUNTRY}/
+FL_QUALITY_ANALYSIS_OUTPUT=$(baseDir)/output
+FL_JET_FILE=${FL_COMPILED_ROOT_DIR}/Android/FleksySDKResources/encrypted/resourceArchive-${LANG}-${COUNTRY}.jet
+FL_QP=$(baseDir)/bin/QPMeasure
+FL_SIM_GRAMS=$(baseDir)/bin/SimNGrams
+QP_OUTPUT_FILE=${FL_QUALITY_ANALYSIS_OUTPUT}/QPOutput.txt
+
+mkdir -p ${FL_QUALITY_ANALYSIS_OUTPUT}
+rm -f ${FL_QUALITY_ANALYSIS_OUTPUT}/*
+
+if [[ ${USE_PARLIMENT_SRC} -eq 1 ]]; then
+    PARL_SAMPLE=${FL_QUALITY_ANALYSIS_OUTPUT}/parlSample.txt
+    PARL_FILE=`ls -l ${FL_PARLIMENT_ASSET_DIR} | awk '{print $9}'`
+    PARL_FILE=`echo ${PARL_FILE}`
+
+    echo "Sampling sentences from the parliment source..."
+    $PYTHON3 ${FL_COMPILED_QUALITY_DIR}/sampleSentences.py ${FL_PARLIMENT_ASSET_DIR}/${PARL_FILE} ${PARL_SRC_SIZE} > \
+	${PARL_SAMPLE}
+    
+    let CNT=1
+    for NOISE in ${QP_NOISE_SEQUENCE}; do
+	echo "Launching QP for noise ${NOISE}"
+	${FL_QP} ${FL_JET_FILE} ${PARL_SAMPLE} 0 20 $NOISE >> ${QP_OUTPUT_FILE}_$NOISE &
+	PROC_IDS[$CNT]=$!
+	if [[ $CNT -eq $NUM_PROCS ]]; then
+	    echo "Waiting for procs..."
+	    waitAllProcs PROC_IDS[@] 10
+	    let CNT=1
+	else
+	    let CNT=CNT+1
+	fi
+    done
+
+    if [[ $CNT -gt 0 ]]; then
+	echo "Waiting for procs..."
+	waitAllProcs PROC_IDS[@] 10
+    fi
+
+    echo "Preparing QP output"
+    for NOISE in ${QP_NOISE_SEQUENCE}; do
+	cat ${QP_OUTPUT_FILE}_$NOISE >> ${QP_OUTPUT_FILE}
+	rm ${QP_OUTPUT_FILE}_$NOISE
+    done
+fi
+
+
--- Quality/sampleSentences.py
+++ Quality/sampleSentences.py
@@ -3,31 +3,49 @@
 import argparse, re, codecs, random, time
 
 parser = argparse.ArgumentParser(description='Given a text file, this will randomly sample '
-                                 'sentences in the text. Outputs to stdout')
+                                 'sentences in the text. Outputs to stdout. The first line '
+                                 'printed is the seed used')
 parser.add_argument('input', help='Text file')
 parser.add_argument('word_count', type=int, help='Desired word count in document')
 parser.add_argument('-l', '--length', type=int, default=3, help = 'Sets the minimum number '
                     'of words in a sentence')
+parser.add_argument('-s', '--seed', type=float, default=-1, help = 'Manually sets the seed')
 args = parser.parse_args()
 
-random.seed(time.clock())
+if (args.seed != -1):
+    seed = args.seed
+else:
+    seed = time.clock()
+
+print(seed)
+random.seed(seed)
 input = codecs.open(args.input, 'r', "utf-8")
 text = ""
 for line in input:
     text += line.strip()
 
 sentences = re.split("\.", text)
+
+if (len(sentences) == 0):
+    exit()
+
 totalCnt = 0
-while (totalCnt < args.word_count and len(sentences) > 0):
+while (totalCnt <= args.word_count):
     temp = []
     for sent in sentences:
         words = re.split(" ", sent)
         rnd = random.random()
         if (len(words) >= args.length):
-            if (rnd <= 0.1):
-                totalCnt += len(words)
-                print(sent.strip() + ".")
-            else:
-                temp.append(sent)
+            try:
+                if (rnd <= 0.1):
+                    s = sent.strip() + "."
+                    print(s)
+                    totalCnt += len(words)
+                    if (totalCnt > args.word_count):
+                        break
+                else:
+                    temp.append(sent)
+            except UnicodeEncodeError:
+                pass
             
     sentences = list(temp)
