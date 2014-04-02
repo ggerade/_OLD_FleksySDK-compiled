093e818547541816b6bf2e39560c831c1ab3d4aa
Mike Loh <mike@syntellia.com>
2014-04-02 11:56:16 -0700

    script changed to work on word count instead of a threshold

--- Quality/sampleSentences.py
+++ Quality/sampleSentences.py
@@ -1,14 +1,13 @@
+# quick implementation. Not particularly efficient with space
+
 import argparse, re, codecs, random, time
 
 parser = argparse.ArgumentParser(description='Given a text file, this will randomly sample '
                                  'sentences in the text. Outputs to stdout')
 parser.add_argument('input', help='Text file')
-parser.add_argument('threshold', type=float, help='Cutoff level, ie 0.1 means 10% of the sentences '
-                    'will be kept')
+parser.add_argument('word_count', type=int, help='Desired word count in document')
 parser.add_argument('-l', '--length', type=int, default=3, help = 'Sets the minimum number '
                     'of words in a sentence')
-parser.add_argument('-w', '--word_count', type=int, default=3, help='Desired word count '
-                    'in document')
 args = parser.parse_args()
 
 random.seed(time.clock())
@@ -19,16 +18,16 @@ for line in input:
 
 sentences = re.split("\.", text)
 totalCnt = 0
-for sent in sentences:
-    words = re.split(" ", sent)
-    rnd = random.random()
-    if (len(words) >= args.length and rnd <= args.threshold):
-        totalCnt += len(words)
-        print(sent.strip() + ". ")
-
-    if (totalCnt >= args.word_count):
-        break;
-
-
-
-
+while (totalCnt < args.word_count and len(sentences) > 0):
+    temp = []
+    for sent in sentences:
+        words = re.split(" ", sent)
+        rnd = random.random()
+        if (len(words) >= args.length):
+            if (rnd <= 0.1):
+                totalCnt += len(words)
+                print(sent.strip() + ".")
+            else:
+                temp.append(sent)
+            
+    sentences = list(temp)
