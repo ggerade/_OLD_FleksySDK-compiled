e9e49b6d7c55a6b0584e0daa638d7d7da9a87905
Mike Loh <mike@syntellia.com>
2014-04-03 16:39:00 -0700

    adding SImNGrams to analysis

--- Quality/runQualityAnalysis.sh
+++ Quality/runQualityAnalysis.sh
@@ -13,7 +13,8 @@ function runQPMeasure()
     let CNT=1
     for NOISE in ${QPM_NOISE_SEQUENCE}; do
 	echo "Launching QP for noise ${NOISE}"
-	${FL_QP} ${FL_JET_FILE} ${QPM_SAMPLE} 0 ${NOISE} ${QPM_RUN_SIZE} >> ${QPM_OUTPUT_FILE}_$NOISE &
+	${FL_QP} ${FL_JET_FILE} ${QPM_SAMPLE} 0 ${NOISE} ${QPM_TRIALS} >> \
+	    ${QPM_OUTPUT_FILE}_$NOISE &
 	PROC_IDS[$CNT]=$!
 	if [[ $CNT -eq $NUM_PROCS ]]; then
 	    echo "Waiting for procs..."
@@ -33,6 +34,40 @@ function runQPMeasure()
     for NOISE in ${QPM_NOISE_SEQUENCE}; do
 	cat ${QPM_OUTPUT_FILE}_$NOISE >> ${QPM_OUTPUT_FILE}
 	rm ${QPM_OUTPUT_FILE}_$NOISE
+    done
+}
+
+function runSimNGrams()
+{
+    TYPE=$1
+    FILE=$2
+
+    echo "Simulating NGrams for ${TYPE} with file ${FILE}"
+    LENGTH=`wc -l $FILE | awk '{print $1}'`
+    SUB_LENGTH=$(( $LENGTH/${NUM_PROCS} ))
+
+    for i in `seq 1 ${NUM_PROCS}`; do 
+	START=$(( ($i-1)*$SUB_LENGTH + 1 ))
+	END=$(( $START + $SUB_LENGTH - 1 ))
+	sed -n "$START,${END}p" $FILE > ${FL_QUALITY_ANALYSIS_OUTPUT}/_${i}.txt
+    done
+
+    for i in `seq 1 ${NUM_PROCS}`; do
+	${FL_SIM_GRAMS} ${FL_JET_FILE} ${FL_QUALITY_ANALYSIS_OUTPUT}/_${i}.txt \
+	    ${SG_TRIALS} 1 ${SG_OFFSET} ${SG_STD} > \
+	    ${SG_OUTPUT}_$i &
+	ID[$i]=$!
+    done
+
+    echo "waiting for processes to finish...."
+    for i in `seq 1 ${NUM_PROCS}`; do
+	waitProc ${ID[$i]} 10
+    done
+
+    for i in `seq 1 ${NUM_PROCS}`; do
+	rm ${FL_QUALITY_ANALYSIS_OUTPUT}/_${i}.txt
+	cat ${SG_OUTPUT}_$i >> ${SG_OUTPUT}
+	rm ${SG_OUTPUT}_$i
     done
 }
 
@@ -69,18 +104,28 @@ mkdir -p ${FL_QUALITY_ANALYSIS_OUTPUT}
 rm -f ${FL_QUALITY_ANALYSIS_OUTPUT}/*
 
 if [[ ${USE_PARLIMENT_SRC} -eq 1 ]]; then
-
     PARL_FILE=`ls -l ${FL_PARLIMENT_ASSET_DIR} | awk '{print $9}'`
     PARL_FILE=`echo ${PARL_FILE}`
 
     # set up variables for runQPMeasure to work
     QPM_SAMPLE=${FL_QUALITY_ANALYSIS_OUTPUT}/parlSample.txt
     QPM_FILE=${FL_PARLIMENT_ASSET_DIR}/${PARL_FILE}
-    QPM_SRC_SIZE=${PARL_SRC_SIZE}
-    QPM_NOISE_SEQUENCE=${QP_NOISE_SEQUENCE}
-    QPM_RUN_SIZE=${QP_RUN_SIZE}
-    QPM_OUTPUT_FILE=${FL_QUALITY_ANALYSIS_OUTPUT}/QPOutput.txt
+    QPM_SRC_SIZE=${PARL_QP_SRC_SIZE}
+    QPM_NOISE_SEQUENCE=${PARL_QP_NOISE_SEQUENCE}
+    QPM_TRIALS=${PARL_QP_TRIALS}
+    QPM_OUTPUT_FILE=${FL_QUALITY_ANALYSIS_OUTPUT}/QPParlOutput.txt
     runQPMeasure parliment
+
+    SG_OFFSET=${PARL_SG_OFFSET}
+    SG_STD=${PARL_SG_STD}
+    SG_TRIALS=${PARL_SG_TRIALS}
+    SG_OUTPUT=${FL_QUALITY_ANALYSIS_OUTPUT}/SGParlBigrams.txt    
+    echo ""
+    runSimNGrams parliment ${FL_QUALITY_ANALYSIS_DATA}/parlBigrams.txt
+
+    SG_OUTPUT=${FL_QUALITY_ANALYSIS_OUTPUT}/SGParlTrigrams.txt    
+    echo ""
+    runSimNGrams parliment ${FL_QUALITY_ANALYSIS_DATA}/parlTrigrams.txt
 fi
 
 
