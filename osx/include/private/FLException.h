4c7840f593a114c164b4069f8c7d345b70f5a192
Vadim Maximov <vadim@syntellia.com>
2014-04-10 15:15:10 -0700

    v0.76 Source: https://github.com/syntellia/FleksySDK-source/commit/76fbbd9e39e73c0650688f1416a6d362fd929e11

--- osx/include/private/FLException.h
+++ osx/include/private/FLException.h
@@ -14,72 +14,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sstream>
 
 #include "FleksySDKVersion.h"
 
 extern "C" {
-extern char *(*fl_backtrace)(void);
+  extern char *(*fl_backtrace)(void);
 }
 
 namespace Fleksy {
-
-class exception : public std::exception {
-
-protected:
-  std::string message_;
-  std::string whatString_;
-  std::string backtraceString_;
-  std::string fileString_;
-  int lineNumber_ = 0;
-
-  void init(const char *file, int lineNumber, std::string msg) {
-    backtraceString_ = fl_backtrace();
-    if(file != NULL) {
-      fileString_ = file;
-    }
-    lineNumber_ = lineNumber;
-    message_ = msg;
+  
+  class exception : public std::exception {
     
-    std::stringstream ss;
-    if (!fileString_.empty()) {
-      ss << "<" << fileString_ << ":" << lineNumber_ << "> ";
+  protected:
+    char *message_         = NULL;
+    char *whatCString_     = NULL;
+    char *backtraceString_ = NULL;
+    char *fileString_      = NULL;
+    int   lineNumber_      = 0;
+    
+    void init(const char *file, int lineNumber, const char *message) {
+      backtraceString_ = fl_backtrace();
+      if(file != NULL) { fileString_ = strdup(file); }
+      lineNumber_ = lineNumber;
+      asprintf(&message_, "%s", message);
+      if(fileString_ != NULL) { asprintf(&whatCString_, "<%s:%d> %s\nVersion: %s-%s@%s\n%s", fileString_, lineNumber_, message == NULL ? "<NULL>" : message, fleksySDKBuildGitShortHash, fleksySDKBuildUser, fleksySDKBuildDate, backtraceString_ == NULL ? "<UNAVAILABLE>" : backtraceString_); }
+      else                    { asprintf(&whatCString_,         "%s\nVersion: %s-%s@%s\n%s",                           message == NULL ? "<NULL>" : message, fleksySDKBuildGitShortHash, fleksySDKBuildUser, fleksySDKBuildDate, backtraceString_ == NULL ? "<UNAVAILABLE>" : backtraceString_); }
     }
-    ss << (message_.empty() ? "<NO MESSAGE>" : message_) << "\n";
-    ss << "Version: " << fleksySDKBuildGitShortHash << "-" << fleksySDKBuildUser << "@" << fleksySDKBuildDate << "\n";
-    ss << backtraceString_;
-  }
+    void vinit(const char *file, int lineNumber, const char *message, va_list args) {
+      char *vmessage = NULL;
+      vasprintf(&vmessage, message == NULL ? "<NULL>" : message, args);
+      init(file, lineNumber, vmessage);
+      if(vmessage != NULL) { free(vmessage); vmessage = NULL; }
+    }
+    
+  public:
+    
+    explicit exception(const std::string &message)                                   { init(NULL,         -1, message.c_str()); }
+    explicit exception(const char *file, int lineNumber, const std::string &message) { init(file, lineNumber, message.c_str()); }
+    explicit exception(const char *message, ...)                                     { va_list args; va_start(args, message); vinit(NULL,         -1, message, args); va_end(args); }
+    explicit exception(const char *file, int lineNumber, const char *message, ...)   { va_list args; va_start(args, message); vinit(file, lineNumber, message, args); va_end(args); }
+    virtual ~exception() throw () {
+      if(whatCString_     != NULL) { free((void *)whatCString_);     whatCString_     = NULL; }
+      if(backtraceString_ != NULL) { free((void *)backtraceString_); backtraceString_ = NULL; }
+      if(fileString_      != NULL) { free((void *)fileString_);      fileString_      = NULL; }
+      if(message_         != NULL) { free((void *)message_);         message_         = NULL; }
+    }
+    virtual const char *what() const throw () { return(whatCString_); }
+    virtual const char *message() const throw () { return message_; }
+  };
   
-  void vinit(const char *file, int lineNumber, const char *message, va_list args) {
-    char *vmessage = NULL;
-    vasprintf(&vmessage, message == NULL ? "<NULL>" : message, args);
-    init(file, lineNumber, vmessage);
-    if(vmessage != NULL) { free(vmessage); vmessage = NULL; }
-  }
-
-public:
-
-  explicit exception(const std::string &message)                                   { init(NULL,         -1, message.c_str()); }
-  explicit exception(const char *file, int lineNumber, const std::string &message) { init(file, lineNumber, message.c_str()); }
-  explicit exception(const char *message, ...)                                     { va_list args; va_start(args, message); va_end(args); vinit(NULL,         -1, message, args); }
-  explicit exception(const char *file, int lineNumber, const char *message, ...)   { va_list args; va_start(args, message); va_end(args); vinit(file, lineNumber, message, args); }
-  
-  virtual const char *what() const throw () {
-    return(whatString_.c_str());
-  }
-  
-  std::string message() const {
-    return message_;
-  }
-  
-  std::string backtrace() const {
-    return backtraceString_;
-  }
-};
-
 }
 
 #define FLException(msg, ...) Fleksy::exception(__FILE__, __LINE__, msg, ##__VA_ARGS__)
-#define EXassert(condition) if (!(condition)) { throw FLException("Assertion failed! <" #condition ">"); }
 
 #endif /* defined(__FleksySDK__FLException__) */
